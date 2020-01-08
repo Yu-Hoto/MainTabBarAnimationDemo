fb3a87e702615ed7d153a90c4d97affedfaac6b0
Yu-Hoto <yu.hoto.0220@gmail.com>
2020-01-08 16:08:12 +0900

    Initial Commit

---
+++ MainTabbarAnimationDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MainTabbarAnimationDemo
+//
+//  Created by Yu Hoto on 2020/01/08.
+//  Copyright © 2020 Yu Hoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MainTabbarAnimationDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MainTabbarAnimationDemo
+//
+//  Created by Yu Hoto on 2020/01/08.
+//  Copyright © 2020 Yu Hoto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
